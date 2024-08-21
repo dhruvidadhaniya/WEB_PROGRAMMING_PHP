d5e3441091984dd302a1269c28f79edf0c7674ef
dhruvidadhaniya <dhruvidadhaniya7@gmail.com>
2024-08-21 15:54:52 +0530

    Add files via upload

--- task1/script6.php
+++ task1/script6.php
@@ -1,1 +1,10 @@
-
+<?php
+echo "<b>dhruvi dadhaniya</b><br>";
+echo "<i>dhruvi dadhaniya</i><br>";
+echo "<h1>dhruvi dadhaniya</h1><br>";
+echo "<h2>dhruvi dadhaniya</h2><br>";
+echo "<h3>dhruvi dadhaniya</h3><br>";
+echo "<h4>dhruvi dadhaniya</h4><br>";
+echo "<h5>dhruvi dadhaniya</h5><br>";
+echo "<h6>dhruvi dadhaniya</h6><br>";
+?>
