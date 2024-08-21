39b1260e3ac673a435ab1f7e4269ea854c246ee2
dhruvidadhaniya <dhruvidadhaniya7@gmail.com>
2024-08-21 15:57:17 +0530

    Add files via upload

--- task1/script9.php
+++ task1/script9.php
@@ -1,1 +1,10 @@
-
+<?php
+$name="dhruvi";
+$age=20;
+$city="rajkot";
+$mobile=9538764585;
+echo "my name is  ".$name."<br>";
+echo "my age is  ".$age."<br>";
+echo "my city is  ".$city."<br>";
+echo "my mobile no is ".$mobile;
+?>
